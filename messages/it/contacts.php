3415228f477c58c823d1932057a4dc0d5f764f5b
Giandomenico Olini <giando.olini@gogodigital.it>
2017-05-28 11:44:52 +0200

    Adding messages

---
+++ messages/it/contacts.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @copyright Copyright &copy; Gogodigital Srls
+ * @company Gogodigital Srls - Wide ICT Solutions
+ * @website http://www.gogodigital.it
+ * @github https://github.com/cinghie/yii2-contacts
+ * @license GNU GENERAL PUBLIC LICENSE VERSION 3
+ * @package yii2-contacts
+ * @version 0.1.0
+ */
+
+return [
+    'All' => 'Tutte',
+    'Dashboard'=>'Dashboard',
+    'Delete'=>'Cancella',
+    'Yes'=>'Si',
+    'No'=>'No',
+    'New'=>'Nuovo',
+    'Update'=>'Modifica',
+];
