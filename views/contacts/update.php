dde577fbc00791a4025ebf27d962b469d36d85ca
Giandomenico Olini <giando.olini@gogodigital.it>
2017-06-01 18:18:27 +0200

    Update breadcrumbs

--- views/contacts/update.php
+++ views/contacts/update.php
@@ -12,7 +12,7 @@ use kartik\helpers\Html;
 
 $this->title = \Yii::t('contacts', 'Update Contacts') . ": " .$model->firstname ." ". $model->lastname;
 $this->params['breadcrumbs'][] = ['label' => \Yii::t('contacts', 'Contacts'), 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->firstname, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = ['label' => $model->firstname. " " . $model->lastname, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = \Yii::t('contacts', 'Update');
 ?>
 <div class="contacts-update">
