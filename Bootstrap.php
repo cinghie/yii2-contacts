ac14684b65f341f8a231d36fa9587e2a38676a0e
Giandomenico Olini <giando.olini@gogodigital.it>
2019-07-11 13:25:24 +0200

    Update version

--- Bootstrap.php
+++ Bootstrap.php
@@ -7,7 +7,7 @@
  * @github https://github.com/cinghie/yii2-contacts
  * @license GNU GENERAL PUBLIC LICENSE VERSION 3
  * @package yii2-contacts
- * @version 0.9.6
+ * @version 0.9.7
  */
 
 namespace cinghie\contacts;
