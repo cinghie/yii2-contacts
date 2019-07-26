764497721c0b436cb549e79a4ba7f3f93a1e2749
Giandomenico Olini <giando.olini@gogodigital.it>
2019-07-26 16:02:43 +0200

    Adding Contact message table

---
+++ migrations/m180620_100457_create_contacts_messages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * @copyright Copyright &copy; Gogodigital Srls
+ * @company Gogodigital Srls - Wide ICT Solutions
+ * @website http://www.gogodigital.it
+ * @github https://github.com/cinghie/yii2-contacts
+ * @license GNU GENERAL PUBLIC LICENSE VERSION 3
+ * @package yii2-contacts
+ * @version 0.9.7
+ */
+
+use cinghie\traits\migrations\Migration;
+
+class m180620_100457_create_contacts_messages_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $this->createTable('{{%contacts}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(100)->notNull(),
+            'email' => $this->string(100)->notNull(),
+            'message' => $this->text()->notNull(),
+        ], $this->tableOptions);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropTable('{{%contacts_messages}}');
+    }
+}
