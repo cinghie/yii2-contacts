de42c38937913647e8ea64e7a4fcb0bf101302c3
Giandomenico Olini <giando.olini@gogodigital.it>
2019-09-04 15:13:14 +0200

    Adding FormWidget

---
+++ widgets/FormWidget.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @copyright Copyright &copy; Gogodigital Srls
+ * @company Gogodigital Srls - Wide ICT Solutions
+ * @website http://www.gogodigital.it
+ * @github https://github.com/cinghie/yii2-contacts
+ * @license GNU GENERAL PUBLIC LICENSE VERSION 3
+ * @package yii2-contacts
+ * @version 0.9.7
+ */
+
+namespace cinghie\contacts\widgets;
+
+use cinghie\contacts\models\Forms;
+use cinghie\contacts\models\Messages;
+
+/**
+ * Class FormWidget
+ */
+class FormWidget
+{
+	/**
+	 * @var int $id
+	 */
+	public $id;
+
+	/**
+	 * Widget Init
+	 */
+	public function init()
+	{
+		parent::init();
+	}
+
+	/**
+	 * @return string
+	 */
+	public function run()
+	{
+		return 'Hello World!';
+	}
+}
